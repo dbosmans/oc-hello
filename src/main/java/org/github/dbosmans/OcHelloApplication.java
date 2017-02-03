d826ffcb7d0a0cb56fe363c5f6ea046730729cc5
Diederik Bosmans <diederik.bosmans@gmail.com>
2017-02-03 20:03:58 +0100

    generating project

---
+++ src/main/java/org/github/dbosmans/OcHelloApplication.java
@@ -0,0 +1,12 @@
+package org.github.dbosmans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcHelloApplication.class, args);
+	}
+}
